52f4cd888f4e9afd849a813faadd543c3405fc72
Jessybr <jessicabuenoramos5@gmail.com>
2023-12-21 17:40:21 -0300

    ✨update files

--- script/categ.js
+++ script/categ.js
@@ -16,9 +16,11 @@ class Receita {
     card() {
         conteiner.innerHTML = `<div class="card">
             <img class="card-img" src="images/${this.imag}.png" alt="${this.nome}">
-            <h3 class="card-title">${this.nome}</h3>
+            <div class="tituloRec">
+                <h3 class="card-title">${this.nome}</h3>
+            </div>
             <div class="botao">
-                <a href="" class="card-link" style="color: var(--marromCla);">Receita</a>
+                <button id="btnVerRec" style="color: var(--marromCla);">Receita</button>
                 <img src="images/iconReceita.png" alt="icone receita">
             </div>
         </div>`;
@@ -31,6 +33,24 @@ class Receita {
 
 Receita.instances = [];
 
+// const btnSub=document.getElementById("btnSub");
+
+// btnSub.addEventListener("click", ()=>{
+//     let nome=document.getElementById("nome").value;
+//     let tempo=document.getElementById("tempo").value;
+//     let rendi=document.getElementById("rendi").value;
+//     let dific=document.getElementById("dific").value;
+//     let ingred=document.getElementById("ingred").value;
+//     let modo=document.getElementById("modo").value;
+//     let categ=document.getElementById("categ").value;
+//     let imag=document.getElementById("imag").value;
+   
+//     let novaRes = new Receita(nome, tempo, rendi, dific, ingred, modo, categ, imag);
+
+// })
+
+
+
 const ingredTofu = ["500g de tofu em cubos", "3 colheres (sopa) de farinha de trigo", "2 colheres (sopa) de óleo de soja", "2 cenouras raladas grosseiramente", "1 xícara (chá) de vagem em pedaços", "1 pimentão vermelho em tiras", "2 colheres (sopa) de molho de soja (shoyu)", "Sal e pimenta-do-reino a gosto", "Cebolinha verde para decorar", "Gergelim branco torrado para polvilhar"];
 const tofu = new Receita("Tofú Oriental com Vegetais", "30 minutos", "6 porções", "Fácil", ingredTofu, "Passe o tofu na farinha de trigo...", "Pratos Quentes", "tofu-oriental-vegetais");
 
@@ -41,13 +61,19 @@ const ingredBatatDcFrita = ["2 batatas-doces", "Óleo para fritar", "Sal a gosto
 const batatDcFrita = new Receita("Batata Doce Frita", "20 minutos", "4 porções", "Fácil", ingredBatatDcFrita, "Lave as batatas, descasque e fatie-as...", "Pratos Frios", "batata-doce-frita");
 
 const ingreSucoMelancMoran = ["300ml de água de coco", "1 xícara (chá) de melancia em cubos", "1/2 xícara (chá) de amoras", "6 morangos", "1/2 xícara (chá) de mel", "Gelo a gosto"];
-const sucoMelancMoran = new Receita("Suco de Melancia com Morango", "15 minutos", "3 porções", "Fácil", ingreSucoMelancMoran, "No liquidificador, bata a água de coco, a melancia, as amoras, os morangos, o mel e gelo a gosto até ficar homogêneo. Coe e sirva em seguida, se desejar, em copos decorados com morangos.", "Sucos", "suco-melancia-morango");
+const sucoMelancMoran = new Receita("Suco de Melancia com Morango", "15 minutos", "3 porções", "Fácil", ingreSucoMelancMoran, "No liquidificador, bata a água de coco, a melancia, as amoras, os morangos, o mel e gelo a gosto até ficar homogêneo. Coe e sirva em seguida, se desejar, em copos decorados com morangos.", "Bebidas", "suco-melancia-morango");
 
 const ingrePaoQueijo = ["1 xícara de chá de batata cozida", "2 xícaras de chá de polvilho doce", "1/2 xícara de chá de polvilho azedo", "1/2 xícara de chá de água morna", "1/3 de xícara de chá de azeite", "Sal a gosto"];
 const paoQueijo = new Receita("Pão de Queijo", "30 minutos", "25 porções", "Médio", ingrePaoQueijo, "Em um recipiente, coloque a batata cozida e amasse bem com um garfo até ficar um purê bem macio.", "Aperitivos", "pao-de-queijo");
 
 const ingreBoloLaranjaA = ["120 g de mix de farinhas sem glúten", "100 g de farelo de aveia", "150 g de açúcar demerara", "70 ml de suco de laranja", "180 ml de água", "100 ml de azeite de oliva", "2 ramos de alecrim sem o talo", "12 g de fermento químico", "1 colher de sopa de vinagre de maçã", "1 pitada de sal", "Açúcar a gosto"];
 const boloLaranjaA = new Receita("Bolo de Laranja com Alecrim", "45 minutos", "10 porções", "Médio", ingreBoloLaranjaA, "Em uma tigela, misture a farinha com o farelo de aveia e reserve.", "Aperitivos", "bolo-de-laranja-com-alecrim");
+
+const ingrePanqueca = ["3 xícaras de chá de água", "1/2 xícara de chá de óleo vegetal", "3 xícaras de chá de farinha de trigo", "Sal a gosto", "1 colher de chá de fermento químico em pó (fermento para bolo)"];
+const panqueca = new Receita("Panqueca Vegana", "30 minutos", "5 porções", "Médio", ingrePanqueca, "Em um liquidificador, adicione a água, o óleo, a farinha de trigo e o sal. Bata até obter uma massa homogênea e lisa;", "Pratos Quentes", "panqueca");
+
+const ingreChoq = ["500 ml de leite vegetal de sua preferência", "2 colheres de sopa de amido de milho", "2 colheres de sopa de cacau em pó", "Açúcar a gosto", "2 quadradinhos de chocolate vegano"];
+const choq = new Receita("Chocolate quente vegano", "15 minutos", "2 porções", "Fácil", ingrePanqueca, "Em uma panela, coloque o leite, o amido de milho e misture bem até dissolver.", "Bebidas", "chocolate-quente");
 
 let todasReceitas = Receita.getInstances();
 let cardsHTML = "";
